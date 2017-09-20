6451019585ff2709b06056e541ee5c5e4eb3f108
caitsith2 <d_good@caitsith2.com>
2017-09-20 00:16:04 -0700

    Resolve merge conflict of laudry fix.

--- Ktane-Laundry/Assets/Laundry.cs
+++ Ktane-Laundry/Assets/Laundry.cs
@@ -114,7 +114,7 @@ public class Laundry : MonoBehaviour
         int[] SolutionStates = new int[4];
         int solved = BombInfo.GetSolvedModuleNames().Count;
         int ItemClothing = (BombInfo.GetSolvableModuleNames().Count - solved + TotalIndicators + 6) % 6;
-        int ItemMaterial = (TotalPorts + solved - BatteryHolders + 6) % 6;
+        int ItemMaterial = ((TotalPorts + solved - BatteryHolders) % 6 + 6) % 6;
         int ItemColor = (LastDigitSerial + TotalBatteries + 6) % 6;
         LogString.AppendFormat("Clothing: {0} ({1}), Material: {2} ({3}), Color: {4} ({5})\n", ClothingNames[ItemClothing], ItemClothing, MaterialNames[ItemMaterial], ItemMaterial, ColorNames[ItemColor], ItemColor);
 
@@ -354,8 +354,8 @@ public class Laundry : MonoBehaviour
             {"hangtodry",5 }, {"hangdry",5 },
             {"dripdry",6 },
             {"dryflat",7 },
-            {"dryintheshade",8 }, {"dryinshade",8 },,
-            {"donotdry",9 }, {"dontdry",9 },,
+            {"dryintheshade",8 }, {"dryinshade",8 },
+            {"donotdry",9 }, {"dontdry",9 },
             {"donottumbledry",10 }, {"donttumbledry",10 },
             {"dry",11 },
         };
